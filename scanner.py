2688a4a3c35a746edd1915344148d4277ed0ebaf
ASUS <ma.kh.inbox2@gmail.com>
2021-10-17 00:29:19 +0330

    lexical functions completed.

--- scanner.py
+++ scanner.py
@@ -3,9 +3,6 @@ import FileManager.file_writer as fw
 import re
 
 
-# Token = namedtuple('Token', ['type', 'val'])
-
-
 class Scanner:
     state = ['start', 'num', 'word', 'equal', 'twoequal', 'symbol', 'slash', 'bcmt', 'bcmt*', 'lcmt', 'wspace', 'star']
     valid_tokens = '[a-zA-Z0-9;:,\[\]\(\)\{\}\+\-\*=< \n\r\t\v\f]'
@@ -112,25 +109,158 @@ class Scanner:
             self.current_token_lexeme = ''
 
     def update_equal_with_char(self):
-        pass
+        if re.search(self.current_char, Scanner.digit):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.letter):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.symbol):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '='):
+            self.current_state = 'twoequal'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, '\*'):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '/'):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.wspace):
+            self.current_state = 'start'
+            self.read_again = True
+        else:
+            self.current_state = 'start'
+            self.current_token_lexeme += self.current_char
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
 
-    def update_tow_equal_with_char(self):
-        pass
+    def update_two_equal_with_char(self):
+        if re.search(self.current_char, Scanner.digit):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.letter):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.symbol):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '='):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '\*'):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '/'):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.wspace):
+            self.current_state = 'start'
+            self.read_again = True
+        else:
+            self.current_state = 'start'
+            self.current_token_lexeme += self.current_char
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
 
     def update_slash_with_char(self):
-        pass
+        if re.search(self.current_char, Scanner.digit):
+            self.current_state = 'start'
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
+        elif re.search(self.current_char, Scanner.letter):
+            self.current_state = 'start'
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
+        elif re.search(self.current_char, Scanner.symbol):
+            self.current_state = 'start'
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
+        elif re.search(self.current_char, '='):
+            self.current_state = 'start'
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
+        elif re.search(self.current_char, '\*'):
+            self.current_state = 'bcmt'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, '/'):
+            self.current_state = 'lcmt'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, Scanner.wspace):
+            self.current_state = 'start'
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
+        else:
+            self.current_state = 'start'
+            self.current_token_lexeme += self.current_char
+            self.error_writer.write_error(self.lineno, '(' + self.current_token_lexeme + ', Invalid input)')
+            self.current_token_lexeme = ''
 
     def update_bcmt_with_char(self):
-        pass
+        self.current_token_lexeme += self.current_char
+        if re.search(self.current_char, '\*'):
+            self.current_state = 'bcmt*'
 
     def update_bcmt_star_with_char(self):
-        pass
+        if re.search(self.current_char, Scanner.digit):
+            self.current_state = 'bcmt'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, Scanner.letter):
+            self.current_state = 'bcmt'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, Scanner.symbol):
+            self.current_state = 'bcmt'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, '='):
+            self.current_state = 'bcmt'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, '\*'):
+            self.current_state = 'bcmt*'
+            self.current_token_lexeme += self.current_char
+        elif re.search(self.current_char, '/'):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.wspace):
+            self.current_state = 'bcmt'
+            self.current_token_lexeme += self.current_char
+        else:
+            self.current_state = 'bcmt'
+            self.current_token_lexeme += self.current_char
 
-    def update_lcmt_star_with_char(self):
-        pass
+    def update_lcmt_with_char(self):
+        if re.search(self.current_char, '\n'):
+            self.current_state = 'start'
+            self.read_again = True
+        else:
+            self.current_token_lexeme += self.current_char
 
     def update_white_space_with_char(self):
-        pass
+        if re.search(self.current_char, Scanner.digit):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.letter):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.symbol):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '='):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '\*'):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, '/'):
+            self.current_state = 'start'
+            self.read_again = True
+        elif re.search(self.current_char, Scanner.wspace):
+            self.current_state = 'wspace'
+            self.current_token_lexeme += self.current_char
+        else:
+            self.current_state = 'start'
+            self.error_writer.write_error(self.lineno, '(' + self.current_char + ', Invalid input)')
+            self.current_token_lexeme = ''
 
     def update_state(self):
         if self.current_state == 'start':
@@ -142,15 +272,15 @@ class Scanner:
         elif self.current_state == 'equal':
             self.update_equal_with_char()
         elif self.current_state == 'twoequal':
-            self.update_tow_equal_with_char()
+            self.update_two_equal_with_char()
         elif self.current_state == 'slash':
-            self.update_tow_equal_with_char()
+            self.update_slash_with_char()
         elif self.current_state == 'bcmt':
             self.update_bcmt_with_char()
         elif self.current_state == 'bcmt*':
             self.update_bcmt_star_with_char()
         elif self.current_state == 'lcmt':
-            self.update_lcmt_star_with_char()
+            self.update_lcmt_with_char()
         elif self.current_state == 'wspace':
             self.update_white_space_with_char()
 
