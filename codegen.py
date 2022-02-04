d9aeee9fa973429109366753781b9023e9d65ea4
GildedJson <sms.tabaian@gmail.com>
2022-02-03 23:49:40 -0800

    First test hopefully done. mk.18

--- codegen.py
+++ codegen.py
@@ -276,7 +276,9 @@ class Codegen:
                 self.program_block.append(f'(SUB, {self.CALL_STACK_HEAD}, #4, {self.CALL_STACK_HEAD})')
                 self.program_block.append(f'(ASSIGN, @{self.CALL_STACK_HEAD}, {address}, )')
             self.compile_time_address_call_stack_counter.pop()
-            self.semantic_stack.append(self.RETURN_VALUE_ADDRESS)
+            new_temp_address = self.get_temp()
+            self.program_block.append(f'(ASSIGN, {self.RETURN_VALUE_ADDRESS}, {new_temp_address}, )')
+            self.semantic_stack.append(new_temp_address)
         elif action_symbol == 'get_function_ready':
             self.ready_function_address = self.semantic_stack[-1]
             for symbol in self.masmal_symbol_table:
