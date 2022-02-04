3d256b9f72170c171f3abd28133353741bab4e01
GildedJson <sms.tabaian@gmail.com>
2022-02-04 01:47:35 -0800

    First test hopefully done. mk.25

--- codegen.py
+++ codegen.py
@@ -283,10 +283,12 @@ class Codegen:
             for paramie in reversed(self.ready_function_param_list):
                 if paramie.pvf == 'pointer':
                     for symbol in self.masmal_symbol_table:
-                        if symbol.address == self.semantic_stack[-1] and symbol.pvf == 'pointer':
-                            self.program_block.append(f'(ASSIGN, {self.semantic_stack.pop()}, {paramie.address}, )')
-                        else:
-                            self.program_block.append(f'(ASSIGN, #{self.semantic_stack.pop()}, {paramie.address}, )')
+                        if symbol.address == self.semantic_stack[-1]:
+                            if symbol.pvf == 'pointer':
+                                self.program_block.append(f'(ASSIGN, {self.semantic_stack.pop()}, {paramie.address}, )')
+                            else:
+                                self.program_block.append(f'(ASSIGN, #{self.semantic_stack.pop()}, {paramie.address}, )')
+                            break
                 else:
                     self.program_block.append(f'(ASSIGN, {self.semantic_stack.pop()}, {paramie.address}, )')
             self.semantic_stack.pop()  # Accidentally didn't use the function name so I'll just pop it and never think about it again
